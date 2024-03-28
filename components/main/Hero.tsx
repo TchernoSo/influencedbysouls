5d781f6a8d79cb8e78b6f1553f8eef0e2190f42b
Tcherno So <tcherno.so@hotmail.com>
2024-03-28 23:40:20 +0000

    back to normal config

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='relative flex flex-col h-full w-full '>
       <video autoPlay loop muted className="w-full h-full object-cover">
-      <source src="./Symbol.mp4" type="video/mp4" />
+      <source src="/Symbol.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
         <HeroContent/>
